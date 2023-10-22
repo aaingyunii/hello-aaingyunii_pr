4e30806da9b3c353b1e15f0407477a9bbc1af28f
aaingyunii <dlsrbs98@gmail.com>
2023-10-23 03:35:24 +0900

    works in python

---
+++ src/hello_aaingyunii_pr/weather_show.py
@@ -0,0 +1,26 @@
+def weather_show():
+
+    import requests
+    from bs4 import BeautifulSoup
+
+    url = 'https://weather.naver.com/'
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            temperature_element = weather_info.find('strong', class_='current')
+            # 요소에서 <span class="blind"> 부분 삭제
+            for span in temperature_element('span'):
+                span.decompose()
+                
+            temperature = temperature_element.text.strip()
+            weather_condition = weather_condition = weather_info.find('span', class_='weather').text
+            
+            print(f'현재 날씨: {weather_condition}')
+            print(f'현재 온도: {temperature}°')
+        else:
+            print('날씨 정보를 가져올 수 없습니다.')
+    except Exception as e:
+        print(f'오류 발생: {str(e)}')
