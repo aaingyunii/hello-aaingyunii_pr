e8b49520a12f9b69be93ee9a5100f23d48e2fd0b
aaingyunii <dlsrbs98@gmail.com>
2023-10-23 11:05:43 +0900

    deploy 1.0.0

--- src/hello_aaingyunii_pr/weather_show.py
+++ src/hello_aaingyunii_pr/weather_show.py
@@ -9,6 +9,7 @@ def weather_show():
         response = requests.get(url)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
+            weather_info = soup.find('div', class_='weather_now')
 
             temperature_element = weather_info.find('strong', class_='current')
             # 요소에서 <span class="blind"> 부분 삭제
@@ -18,8 +19,8 @@ def weather_show():
             temperature = temperature_element.text.strip()
             weather_condition = weather_condition = weather_info.find('span', class_='weather').text
             
-            print(f'현재 날씨: {weather_condition}')
-            print(f'현재 온도: {temperature}°')
+            print(f"\n현재 날씨: {weather_condition}")
+            print(f"현재 온도: {temperature}°")
         else:
             print('날씨 정보를 가져올 수 없습니다.')
     except Exception as e:
--- tests/test_module.py
+++
@@ -1,5 +0,0 @@
-from hello_aaingyunii_pr.module_check import module_check
-
-def test_module():
-    module_check()
-    assert True
